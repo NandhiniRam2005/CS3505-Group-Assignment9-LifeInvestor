407e5409a2b40d1783dbd26659aba03e878a2bcc
Fresno14576 <jdhamilton20@icloud.com>
2025-04-10 13:34:45 -0600

    Set default button disabled colors

--- main.cpp
+++ main.cpp
@@ -17,6 +17,8 @@ int main(int argc, char *argv[])
 
     palette.setColor(QPalette::Button, QColor(220, 156, 0));  // Yellow button background
     palette.setColor(QPalette::ButtonText, QColor(0, 0, 0));    // Black button text
+    palette.setColor(QPalette::Disabled, QPalette::Button, QColor(165, 139, 77)); // Lighter button disabled background
+    palette.setColor(QPalette::Disabled, QPalette::ButtonText, QColor(70, 70, 70));    // Lighter button disabled baground
 
     // Apply the palette to the application
     a.setPalette(palette);
