0e5984b71c79c4509c10af22663f89b6884c8bdb
jacobanderson <jacobAnderson931@gmail.com>
2025-04-15 18:33:27 -0600

    document LifeEvent

--- lifeeventhandler.h
+++ lifeeventhandler.h
@@ -1,8 +1,24 @@
 #ifndef LIFEEVENTHANDLER_H
 #define LIFEEVENTHANDLER_H
+/*
+The header file for LifeEventHandler.
 
+This class represents a Handles life events. It will randomly generate a life event when requested and return it.
+Life events have text information about the event, the amount of money the user's balance should change by, the type of the event
+and the image to display when the event happens.
+
+By Joel Rodriguez, Jacob Anderson,
+Adharsh Ramakrishnan, Nandhini Ramanathan,
+Jake Heairld, Joseph Hamilton
+
+April 22, 2025
+*/
 #include <QObject>
 
+/**
+ * @brief struct holding information about a life event. Including: text representing the event, the price of the event,
+ * the type of the event, and an image to display when the event ocurrs.
+ */
 struct LifeEvent
 {
     std::string text;
@@ -18,11 +34,18 @@ class LifeEventHandler: public QObject
 public:
     explicit LifeEventHandler(QObject *parent = nullptr);
 
+    /**
+     * Generates a random life event
+     * @return A LifeEvent struct containing information about the event.
+     */
     LifeEvent getRandomLifeEvent();
 
 private:
     std::string filePath;
     std::vector<LifeEvent> lifeEvents;
+    /**
+     * Loads all life events into a vector so they can be randomly selected later.
+     */
     void loadAllLifeEvents();
 
 };
